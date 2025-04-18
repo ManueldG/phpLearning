e1cb44850b0f5db37b2b35dc35deb7c127aa88bd
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2025-04-18 17:45:14 +0200

    cache

---
+++ storage/framework/views/36ddf052eec6b53f37f496e47acefeaa.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH C:\Users\Manuel\OneDrive\Pictures\Documents\Sviluppo\multidb\api\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/subcopy.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/3a163362e64bb129b399fd536e8d06c5.php
@@ -0,0 +1,14 @@
+<?php echo strip_tags($header ?? ''); ?>
+
+
+<?php echo strip_tags($slot); ?>
+
+<?php if(isset($subcopy)): ?>
+
+<?php echo strip_tags($subcopy); ?>
+
+<?php endif; ?>
+
+<?php echo strip_tags($footer ?? ''); ?>
+
+<?php /**PATH C:\Users\Manuel\OneDrive\Pictures\Documents\Sviluppo\multidb\api\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/layout.blade.php ENDPATH**/ ?>
