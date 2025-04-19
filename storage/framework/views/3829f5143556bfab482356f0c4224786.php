f38c0a8720d41b2a0b1acd604777909b8442bdad
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2025-04-19 22:19:35 +0200

    cache view

---
+++ storage/framework/views/3829f5143556bfab482356f0c4224786.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH C:\Users\Manuel\OneDrive\Pictures\Documents\Sviluppo\multidb\api\resources\views/vendor/mail/text/header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e143297d6f9765b40b6265c9fe7ab28b.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH C:\Users\Manuel\OneDrive\Pictures\Documents\Sviluppo\multidb\api\resources\views/vendor/mail/text/subcopy.blade.php ENDPATH**/ ?>
