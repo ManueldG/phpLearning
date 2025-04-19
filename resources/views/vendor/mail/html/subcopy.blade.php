717de5300bdf364dddc87019953072c6a07dbd7e
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2025-04-19 22:17:00 +0200

    added Mail model

---
+++ resources/views/vendor/mail/html/subcopy.blade.php
@@ -0,0 +1,8 @@
+<table class="subcopy" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td>
+{{-- Illuminate\Mail\Markdown::parse($slot) --}}
+{{ $slot }}
+</td>
+</tr>
+</table>
