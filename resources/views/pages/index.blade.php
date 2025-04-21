50f252df95c4ed0bc3f35063e50b476f03bf1eb0
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2025-04-21 10:50:37 +0200

    fixed n° lesson

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,11 @@
 <x-profile-layout>
 
+    @if (session('status'))
+
+        <div class="text-green-500">{{ __(session('status')) }}</div>
+
+    @endif
+
     @auth
         @if(Auth::user()->id == 1)
             <div class="my-3 block">
@@ -14,7 +20,7 @@
 
             <!--<div class="flex justify-between gap-8 w-full my-2">-->
 
-                <div class="">{{ $page->id }}</div>
+                <div class="">{{ ++$key }}</div>
                 <div class="">{{ Str::of($page->title)->limit(13) }}</div>
                 <div class="">{{ Str::of($page->description)->limit(40) }}</div>
 
