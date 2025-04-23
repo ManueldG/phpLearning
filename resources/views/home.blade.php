bbca9e9d2101342c88dad2399ef686febfb7e57b
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2025-04-23 11:48:04 +0200

    added section to show error

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
+@if (session('status'))
 
+<div class="text-red-500">{{ __(session('status')) }}</div>
+
+@endif
 @auth
 
     <x-app-layout>
@@ -19,6 +23,7 @@
 
         <div class="py-12">
             <div class="flex flex-row max-w-7xl mx-auto sm:px-6 lg:px-8">
+
                 <x-home />
             </div>
         </div>
