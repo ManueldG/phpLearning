c09e015d2a18b6a2ae7d6f81352151aa3d49dfda
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2025-04-27 10:39:29 +0200

    added missed link

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -4,7 +4,7 @@
         <div class="flex justity-center items-center" >
 
             <ul>
-                <li class=" my-5"><a class="hover:underline text-cyan-700 decoration-cyan-500" href="#">API</a></li>
+                <li class=" my-5"><a class="hover:underline text-cyan-700 decoration-cyan-500" href="{{ route('dashboard') }}">API</a></li>
                 <li class=" my-5"><a class="hover:underline text-cyan-700 decoration-cyan-500" href="{{ route('page.index') }}">Learn</a></li>
                 <li class=" my-5"><a class="hover:underline text-cyan-700 decoration-cyan-500" href="{{ route('console') }}">Console</a></li>
             </ul>
