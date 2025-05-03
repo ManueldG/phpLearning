6e57481a68d72903b8f0c06291f9b6159c6af536
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2025-05-03 21:33:07 +0200

    fixed link to route

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -20,7 +20,7 @@
                     </div>
 
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link href="{{ route('page.show', 1) }}" :active="request()->routeIs('page.index',1)">
+                        <x-nav-link href="{{ route('page.show', 1) }}" :active="request()->routeIs('page.show',1)">
                             {{ __('Tutorial Php') }}
                         </x-nav-link>
                     </div>
@@ -133,7 +133,7 @@
                                     {{ __('Mail') }}
                                 </x-dropdown-link>
 
-                                <x-dropdown-link href="{{ route('page.index') }}">
+                                <x-dropdown-link href="{{ route('page.show',1) }}">
                                     {{ __('Lessons') }}
                                 </x-dropdown-link>
                             @endif
@@ -165,9 +165,9 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="-me-2 flex items-center sm:hidden ">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                    <svg class="size-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg class="size-6 text-black dark:text-white" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
